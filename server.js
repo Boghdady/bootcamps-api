eb5dec264c1e5196d900deebe08db4b400acdf5f
Ahmed Boghdady <boghdady@Ahmeds-iMac.local>
2020-05-04 07:48:42 +0200

    Mongoose connect

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
 // Route files
+const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -18,4 +22,14 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	// Close server & Exit proccess
+	server.close(() => process.exit(1));
+});
