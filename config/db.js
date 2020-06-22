90d1e2e6707e1061af4020f37bee1056303ab315
Ahmed Boghdady <boghdady@Ahmeds-iMac.local>
2020-06-22 05:02:37 +0200

    Add TryCatch

--- config/db.js
+++ config/db.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
+	try {
+		const conn = await mongoose.connect(process.env.MONGODB_URI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
